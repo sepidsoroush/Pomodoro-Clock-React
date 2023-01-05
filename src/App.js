b271aa699d0817db9cd16bd4ed80038fa367c4ab
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-05 17:58:15 +0200

    Add icons to DOM

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
+import { PlayerPlay , PlayerPause , Refresh , Plus , Minus } from 'tabler-icons-react';
+
 
 function App() {
   return (
     <div className="App">
       <div className="setting">
         <label htmlFor="session-range" id="session-label">Work duration</label>
-        <button id="session-decrement">-</button>
+        <button id="session-decrement"><Minus/></button>
         <input type="range" id='session-range' min={5} max={60}  step={1} />
         <span id='session-length'>25</span>
-        <button id="session-increment">+</button>
+        <button id="session-increment"><Plus /></button>
 
         <label htmlFor="break-range" id="break-label">Break duration</label>
-        <button id="break-decrement">-</button>
+        <button id="break-decrement"><Minus/></button>
         <input type="range" id='break-range' min={1} max={30}  step={1} />
         <span id='break-length'>5</span>
-        <button id="break-increment">+</button>
+        <button id="break-increment"><Plus/></button>
       </div>
       <div className="clock">
         <div className='clock-container'>
@@ -23,8 +25,8 @@ function App() {
         </div>
       </div>
       <div className="button-container">
-        <button id='start_stop'></button>
-        <button id='reset'></button>
+        <button id='start_stop'><PlayerPlay/></button>
+        <button id='reset'><Refresh/></button>
       </div>
     </div>
   );
