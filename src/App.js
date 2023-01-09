e37c973edbf9bfb4ede9af235bb371dbdd54a754
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-09 21:47:05 +0200

    import a CDN to pass a test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState , useEffect } from 'react';
 import './App.css';
 import { PlayerPlay , PlayerPause , Refresh , ChevronUp , ChevronDown } from 'tabler-icons-react';
-
+import "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
 
 function App() {
 
